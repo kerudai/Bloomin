3809bfb4c06fa2ccc044442b53748888a4da49fc
LasSparrow <thelassparrow@gmail.com>
2021-03-12 20:34:22 -0800

    push

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Button from '../../comps/Button';
 
 const Login = () => {
 
   return <div className="login">
     Login
+    <Button/>
   </div>
 }
 
