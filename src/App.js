d3bbb419357d1769fbdf9f4f0b4a6a301de56462
juhye7542 <juhye7542@gmail.com>
2021-03-13 02:45:20 -0800

    add posting page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 
 // import Main from 'pages/main';
-import Post from '../src/comps/Post';
+// import Post from '../src/comps/Post';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
 import Profile from 'pages/Profile';
+import Posting from 'pages/Posting';
 
 
 function App() {
@@ -14,13 +15,15 @@ function App() {
       {/* <Main /> */}
 
       {/* Kaye's Comps */}
-      {/* <Post />
+      {/* <Post /> */}
     
-      <Login /> */}
+      {/* <Login /> */}
 
       {/* <Register /> */}
-      
-      <Profile />
+
+      {/* <Profile /> */}
+
+      <Posting />
 
     </div>
   );
