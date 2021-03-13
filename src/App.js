7ae1e2b4b5938a80caa9de41bf8cf0a1605ae985
kaye delos reyes <kayedelosreyes@kayes-MacBook-Pro.local>
2021-03-12 21:16:14 -0800

    Post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
 import Main from 'pages/main';
+import Post from '../src/comps/Post';
 
 function App() {
   return (
     <div className="App">
       <Main />
+
+      {/* Kaye's Comps */}
+      <Post />
     </div>
   );
 }
