b6c390d56ca73860a88eef514407b502cfac6659
John Doe <kerudai@gmail.com>
2021-03-14 21:59:52 -0700

    Edited uploaded page

--- src/pages/Uploaded/index.js
+++ src/pages/Uploaded/index.js
@@ -2,18 +2,23 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Footer from 'comps/Footer';
 import Tophead from 'comps/Tophead';
+import './uploaded.scss';
+import Header from 'comps/Header';
+import P_selectd from 'comps/P_selected';
+import Button from 'comps/Button';
 
 const Container = styled.div`
 padding-top: 85px;
 padding-bottom: 85px;
-background-color: #F5F5F5;
 `;
 
 const Uploaded = () => {
 
-  return <Container>
+  return <Container className="main">
   <Tophead/>
-  
+  <Header headtext="Bloom it goes!"/>
+  <P_selectd />
+  <Button buttontext="POST" />
   <Footer/>
 </Container>
 }
